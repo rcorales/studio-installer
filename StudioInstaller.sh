7aebae148c55e42ed997f757a3401f2263f6fe6d
rcorales <47377687+rcorales@users.noreply.github.com>
2022-11-14 20:24:10 +0800

    Update StudioInstaller.sh

--- StudioInstaller.sh
+++ StudioInstaller.sh
@@ -23,46 +23,46 @@ JDKInstall (){
 	java -version
 }
 
-# Download Android Studio
-DownloadAndroidStudio (){
-	echo "\n Downloading Android Studio \n"
-	wget -c "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.21/android-studio-2021.1.1.21-linux.tar.gz"
-}
+# # Download Android Studio
+# DownloadAndroidStudio (){
+# 	echo "\n Downloading Android Studio \n"
+# 	wget -c "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.21/android-studio-2021.1.1.21-linux.tar.gz"
+# }
 
-# Install Fastboot & ADB Tools
-FastbootADB (){
-	printf "\n Install ADB Tools \n"
-	apt install android-tools-adb android-tools-fastboot -y
-	printf "\n Done installing ADB Tools"
-}
+# # Install Fastboot & ADB Tools
+# FastbootADB (){
+# 	printf "\n Install ADB Tools \n"
+# 	apt install android-tools-adb android-tools-fastboot -y
+# 	printf "\n Done installing ADB Tools"
+# }
 
-# Install Android Studio
-InstallAndroidStudio (){
-	echo "\n Installing Android Studio \n"
-	tar -xzf android-studio-2021.1.1.21-linux.tar.gz -C /opt
+# # Install Android Studio
+# InstallAndroidStudio (){
+# 	echo "\n Installing Android Studio \n"
+# 	tar -xzf android-studio-2021.1.1.21-linux.tar.gz -C /opt
 
-	mkdir -p "$HOME"/.local/share/applications
-	cat > "$HOME"/.local/share/applications/android-studio.desktop <<-EOF
-		[Desktop Entry]
-		Version=2021.1.1.21
-		Type=Application
-		Name=Android Studio
-		Exec="/opt/android-studio/bin/studio.sh" %f
-		Icon=/opt/android-studio/bin/studio.png
-		Categories=Development;IDE;
-		Terminal=false
-		StartupNotify=true
-		StartupWMClass=android-studio
-	EOF
+# 	mkdir -p "$HOME"/.local/share/applications
+# 	cat > "$HOME"/.local/share/applications/android-studio.desktop <<-EOF
+# 		[Desktop Entry]
+# 		Version=2021.1.1.21
+# 		Type=Application
+# 		Name=Android Studio
+# 		Exec="/opt/android-studio/bin/studio.sh" %f
+# 		Icon=/opt/android-studio/bin/studio.png
+# 		Categories=Development;IDE;
+# 		Terminal=false
+# 		StartupNotify=true
+# 		StartupWMClass=android-studio
+# 	EOF
 
-    chmod +x "$HOME"/.local/share/applications/android-studio.desktop
+#     chmod +x "$HOME"/.local/share/applications/android-studio.desktop
 
-	echo "\n Installing Finished \n"
-	rm -rf android-studio*
-	rm -rf StudioInstaller.sh
-}
+# 	echo "\n Installing Finished \n"
+# 	rm -rf android-studio*
+# 	rm -rf StudioInstaller.sh
+# }
 
-JDKInstall
-FastbootADB
-DownloadAndroidStudio
-InstallAndroidStudio
+# JDKInstall
+# FastbootADB
+# DownloadAndroidStudio
+# InstallAndroidStudio
